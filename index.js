91302b46be8530ff45982f5067c1bae2acbf89c3
Sackett <skeesen8@gmail.com>
2023-08-27 19:59:22 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const newArray =[]
+function writeCards(bad, evt) {
+    for (let i = 0; i < 3 ; i++){
+        newArray.push(`Thank you, ${bad[i]}, for the wonderful ${evt} gift!`)}
+    return newArray
+}
+function countDown(number){
+    for (let i = number; i > -1; i--){
+        console.log(i) 
+
+    }
+       }
+
